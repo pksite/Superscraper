e73291751e00af4ba50d60bd281d115079e454cc
pksite <info@pksite.lt>
2025-11-26 11:57:34 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+// PLACEHOLDER MAIN.JS
+// Tik failo struktūrai. Vėliau įdėsiu pilną versiją.
+
+import { Actor } from 'apify';
+
+Actor.main(async () => {
+    await Actor.setValue('RESULT', {
+        status: "PKSite SuperScraper structure created."
+    });
+});
